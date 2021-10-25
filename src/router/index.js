e389ed87613c7b4661f4f8d11b82ff7fb27de151
AlisonV2 <alison.vandromme007@gmail.com>
2021-10-25 08:58:43 +0200

    added global styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home';
+import Artists from '../views/Artists';
+import Conventions from '../views/Conventions';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/artists',
+    name: 'Artists',
+    component: Artists
+  },
+  {
+    path: '/conventions',
+    name: 'Conventions',
+    component: Conventions
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
