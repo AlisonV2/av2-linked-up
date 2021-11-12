285f79218bbe378cf6f2340f941a59ad3ef3e427
AlisonV2 <alison.vandromme007@gmail.com>
2021-11-12 08:09:47 +0100

    worked on e2e tests

--- tests/e2e/specs/client-dash.js
+++ tests/e2e/specs/client-dash.js
@@ -1,7 +1,7 @@
 describe('Client dashboard test', () => {
     it('Test client dashboard', () => {
       cy.visit('/');
-    //   cy.get('#logout-link').click({force: true})
+      // cy.get('#logout-link').click({force: true})
       cy.get('#login-link').click({ force: true });
       cy.get('input[type="email"]').type('alison@test.com');
       cy.get('input[type="password"]').type('alison123');
@@ -18,7 +18,10 @@ describe('Client dashboard test', () => {
       cy.get('.thumbnail-button').click();
       cy.wait(1000);
       cy.get('.bi-envelope').click();
-      cy.get('.message-item').should('have.length', 2)
+      cy.get('.message-item').should('have.length', 2);
+      cy.get('.message-item').eq(0).click({force: true});
+      cy.get('input[type="textarea"]').type('Hello from cypress');
+      cy.get('.btn-outline-secondary').click();
       cy.get('.bi-box-arrow-right').click();
       cy.get('#logout-link').should('not.exist');
     });  
--- tests/e2e/specs/login-flow.js
+++ tests/e2e/specs/login-flow.js
@@ -5,8 +5,8 @@ describe('Login test, check gallery link & Logout', () => {
       cy.get('#login-link').click({ force: true });
       cy.get('.show-signup').click({ force: true });
       cy.get('.show-login').click({ force: true });
-      cy.get('input[type="email"]').type('test@test.com');
-      cy.get('input[type="password"]').type('test123');
+      cy.get('input[type="email"]').type('artist@test.com');
+      cy.get('input[type="password"]').type('artist123');
       cy.get('button').click();
       cy.wait(1000);
       cy.get('.save-btn').should('have.text', 'Save');
