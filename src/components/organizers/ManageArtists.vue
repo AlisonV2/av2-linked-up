e18398ac6e8637bb0bc40d412df60aac01b9a269
AlisonV2 <alison.vandromme007@gmail.com>
2021-11-22 17:07:04 +0100

    updated manage artists and attendees

--- src/components/organizers/ManageArtists.vue
+++ src/components/organizers/ManageArtists.vue
@@ -2,8 +2,9 @@
   <div class="row">
     <div class="col-12" v-for="event in events" :key="event.id">
       <h2 class="title">{{ event.name }}</h2>
+      <p>Artists attending:</p>
       <div v-for="artist in event.stands" :key="artist.userId">
-        {{ artist.userName }}
+        - <router-link :to="{ name: 'Profile', params: { id: artist.userId } }">{{ artist.userName }}</router-link>
       </div>
     </div>
   </div>
@@ -34,6 +35,6 @@ export default {
       .catch((err) => {
         Sentry.captureException(err);
       });
-  }
+  },
 };
 </script>
